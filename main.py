4a5dbcd05569c534efc2bc58e4ce002e9ae80fa3
llimonix <llimonix123@yandex.ru>
2024-11-30 18:33:45 +0300

    change version

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ CLIENT_ID_EN = "1269807014393942046"  # Yandex Music
 CLIENT_ID_RU_DECLINED = "1269826362399522849"  # Яндекс Музыку (склонение для активности "Слушает")
 
 # Версия (tag) скрипта для проверки на актуальность через Github Releases
-CURRENT_VERSION = "v0.2.2"
+CURRENT_VERSION = "v0.2.3"
 
 # Ссылка на репозиторий
 REPO_URL = "https://github.com/FozerG/YandexMusicRPC"
